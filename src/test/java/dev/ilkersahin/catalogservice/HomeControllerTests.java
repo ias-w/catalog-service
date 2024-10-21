038bacca4ef56bdb67588ce5c1fc4558bd59bd4e
İlker Ata Şahin <ilkeratasahin@gmail.com>
2024-10-21 09:38:13 +0300

    2.2 Building cloud native applications with Spring

---
+++ src/test/java/dev/ilkersahin/catalogservice/HomeControllerTests.java
@@ -0,0 +1,24 @@
+package dev.ilkersahin.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome to the book catalog!"));
+    }
+}
